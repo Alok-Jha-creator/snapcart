378d7b67df85be308afadecf6ee79ee0742ceea4
Alok Jha <ajha49993@gmail.com>
2026-09-11 23:27:32 +0545

    update

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,3 +1,8 @@
-export function proxy (){
-    
+import { NextRequest, NextResponse } from "next/server";
+
+export async function proxy (req:NextRequest){
+    const {pathname}= req.nextUrl
+    const publicRoutes= ["/register","/login","/api/auth","/favicon.ico","/_next"]
+      publicRoutes.some((path)=>path==pathname )
+    return NextResponse.next()
 }
