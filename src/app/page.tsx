326d959508586513bc729c322bf6c5a775f286a4
Alok Jha <ajha49993@gmail.com>
2026-09-12 13:11:14 +0545

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import connectDb from '@/lib/db'
+import User from '@/models/user.model'
 import React from 'react'
 
-const Home = () => {
+const Home = async () => {
+  await connectDb()
+  const user =await User.find
   return (
     <div>
-      
+       5:31
     </div>
   )
 }
