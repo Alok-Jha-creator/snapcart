29f33135a43ad826ce92f36ed741fecb47c6056b
Alok Jha <ajha49993@gmail.com>
2026-09-13 00:02:57 +0545

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
+import { auth } from '@/auth'
+import EditRoleMobile from '@/components/EditRoleMobile'
 import connectDb from '@/lib/db'
 import User from '@/models/user.model'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 const Home = async () => {
   await connectDb()
-  const user =await User.find
+  const session = await auth()
+  const user =await User.findById(session?.user?.id)
+  if (!user) {
+   redirect('/login')
+  }
+  const inComplete=!user.mobile || !user.role || (!user.mobile && user.role==='user')
+  if (inComplete) {
+    return <EditRoleMobile/>
+  }
   return (
     <div>
        5:31
